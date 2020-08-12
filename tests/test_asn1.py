590a3419d8429e97dcb81e99fc21ab58d636b22c
Alexander Senier <senier@componolit.com>
2020-08-12 10:14:08 +0200

    Define integer list
    WIP: Componolit/RecordFlux#401

--- tests/test_asn1.py
+++ tests/test_asn1.py
@@ -1,6 +1,6 @@
 import pytest
 
-from rflx.pyrflx import PyRFLX
+from rflx.pyrflx import MessageValue, PyRFLX
 
 SPECS = PyRFLX(["asn1_der.rflx"], True)
 ASN1 = SPECS["ASN1_DER"]
@@ -36,3 +36,21 @@ def test_parse_octet_string() -> None:
     message = ASN1["Message"]
     message.parse(bytes([0x04, 0x04, 0x03, 0x02, 0x06, 0xA0]))
     assert [d.value for d in message.get("Data")] == [0x03, 0x02, 0x06, 0xA0]
+
+
+def test_parse_integers() -> None:
+    def value(message: MessageValue) -> int:
+        m = message
+        while True:
+            yield m.get("Chunk_Value")
+            if m.get("Chunk_Flag") == "False":
+                break
+            m = m.get("Next")
+
+    m1 = ASN1["Integer_List"]
+    m1.parse(bytes([0x82, 0x42]))
+    assert list(value(m1)) == [0x02, 0x42]
+
+    m1 = ASN1["Integer_List"]
+    m1.parse(bytes([0x90, 0xA0, 0xB0, 0xC0, 0x50]))
+    assert list(value(m1)) == [0x10, 0x20, 0x30, 0x40, 0x50]
