202e61edb20187667017fece9b015c016179e5a4
Alexander Senier <senier@componolit.com>
2020-08-23 19:31:25 +0200

    Fix ASN.1 specification

--- tests/test_asn1.py
+++ tests/test_asn1.py
@@ -9,37 +9,37 @@ ASN1 = SPECS["ASN1"]
 
 
 def test_parse_null() -> None:
-    message = ASN1["Message"]
-    message.parse(bytes([5, 0]))
+    message = ASN1["NUL"]
+    message.parse(bytes([0]))
 
 
 def test_parse_integer() -> None:
-    message = ASN1["Message"]
-    message.parse(bytes([2, 1, 47]))
+    message = ASN1["INTEGER"]
+    message.parse(bytes([1, 47]))
     assert message.get("Integer_Flag") == "False"
     assert message.get("Integer_Value") == 47
 
 
 def test_parse_bit_string() -> None:
-    message = ASN1["Message"]
-    message.parse(bytes([0x03, 0x04, 0x06, 0x6E, 0x5D, 0xC0]))
+    message = ASN1["BIT_STRING"]
+    message.parse(bytes([0x04, 0x06, 0x6E, 0x5D, 0xC0]))
     assert message.get("Unused") == 6
-    data = message.get("Data")
+    data = message.get("Value")
     assert isinstance(data, list)
     assert [d.value for d in data] == [0x6E, 0x5D, 0xC0]
 
 
 @pytest.mark.skip(reason="ISSUE: Componolit/RecordFlux#")
 def test_parse_bit_string_invalid_unused() -> None:
-    message = ASN1["Message"]
+    message = ASN1["BIT_STRING"]
     with pytest.raises(ValueError):
-        message.parse(bytes([0x03, 0x04, 0x06, 0x6E, 0x5D, 0xC1]))
+        message.parse(bytes([0x04, 0x06, 0x6E, 0x5D, 0xC1]))
 
 
 def test_parse_octet_string() -> None:
-    message = ASN1["Message"]
-    message.parse(bytes([0x04, 0x04, 0x03, 0x02, 0x06, 0xA0]))
-    data = message.get("Data")
+    message = ASN1["OCTET_STRING"]
+    message.parse(bytes([0x04, 0x03, 0x02, 0x06, 0xA0]))
+    data = message.get("Value")
     assert isinstance(data, list)
     assert [d.value for d in data] == [0x03, 0x02, 0x06, 0xA0]
 
@@ -69,24 +69,24 @@ def test_parse_integers() -> None:
 
 def test_parse_utctime() -> None:
     message = ASN1["UTCTime"]
-    date = "860923175628Z"
+    date = chr(13) + "860923175628Z"
     message.parse(date.encode("ascii"))
     fields = [
-        "Year_H",
-        "Year_L",
-        "Month_H",
-        "Month_L",
-        "Day_H",
-        "Day_L",
-        "Hour_H",
-        "Hour_L",
-        "Minute_H",
-        "Minute_L",
-        "Second_H",
-        "Second_L",
-        "Zulu",
+        "U_Year_H",
+        "U_Year_L",
+        "U_Month_H",
+        "U_Month_L",
+        "U_Day_H",
+        "U_Day_L",
+        "U_Hour_H",
+        "U_Hour_L",
+        "U_Minute_H",
+        "U_Minute_L",
+        "U_Second_H",
+        "U_Second_L",
+        "U_Zulu",
     ]
-    assert [message.get(f) for f in fields] == [ord(d) for d in date]
+    assert [message.get(f) for f in fields] == [ord(d) for d in date[1:]]
 
 
 def test_parse_invalid_utctime() -> None:
@@ -115,7 +115,7 @@ def test_parse_invalid_utctime() -> None:
 
 def test_parse_generalizedtime() -> None:
     message = ASN1["GeneralizedTime"]
-    date = "18860923175628Z"
+    date = chr(15) + "18860923175628Z"
     message.parse(date.encode("ascii"))
     fields = [
         "G_Century_H",
@@ -134,7 +134,7 @@ def test_parse_generalizedtime() -> None:
         "G_Second_L",
         "G_Zulu",
     ]
-    assert [message.get(f) for f in fields] == [ord(d) for d in date]
+    assert [message.get(f) for f in fields] == [ord(d) for d in date[1:]]
 
 
 def test_parse_invalid_generalizedtime() -> None:
@@ -157,108 +157,103 @@ def test_parse_invalid_generalizedtime() -> None:
     ]
     for i, m in invalid:
         with pytest.raises(ValueError):
+            i = chr(13) + i
             print(f"{m}: {i}")
             message.parse(i.encode("ascii"))
 
 
 def test_parse_utctime_message() -> None:
-    message = ASN1["Message"]
+    message = ASN1["UTCTime"]
     date = "860923175628Z"
-    message.parse(bytes([0x17, len(date)]) + date.encode("ascii"))
+    message.parse(bytes([len(date)]) + date.encode("ascii"))
     fields = [
-        "UTCTime_Year_H",
-        "UTCTime_Year_L",
-        "UTCTime_Month_H",
-        "UTCTime_Month_L",
-        "UTCTime_Day_H",
-        "UTCTime_Day_L",
-        "UTCTime_Hour_H",
-        "UTCTime_Hour_L",
-        "UTCTime_Minute_H",
-        "UTCTime_Minute_L",
-        "UTCTime_Second_H",
-        "UTCTime_Second_L",
-        "UTCTime_Zulu",
+        "U_Year_H",
+        "U_Year_L",
+        "U_Month_H",
+        "U_Month_L",
+        "U_Day_H",
+        "U_Day_L",
+        "U_Hour_H",
+        "U_Hour_L",
+        "U_Minute_H",
+        "U_Minute_L",
+        "U_Second_H",
+        "U_Second_L",
+        "U_Zulu",
     ]
     assert [message.get(f) for f in fields] == [ord(d) for d in date]
 
 
 def test_parse_utctime_message2() -> None:
-    message = ASN1["Message"]
+    message = ASN1["UTCTime"]
     data = bytes(
-        [0x17, 0x0D, 0x31, 0x39, 0x31, 0x32, 0x31, 0x36, 0x30, 0x33, 0x30, 0x32, 0x31, 0x30, 0x5A]
+        [0x0D, 0x31, 0x39, 0x31, 0x32, 0x31, 0x36, 0x30, 0x33, 0x30, 0x32, 0x31, 0x30, 0x5A]
     )
     message.parse(data)
-    assert message.get("UTCTime_Year_H") == ord("1")
-    assert message.get("UTCTime_Year_L") == ord("9")
-    assert message.get("UTCTime_Month_H") == ord("1")
-    assert message.get("UTCTime_Month_L") == ord("2")
-    assert message.get("UTCTime_Day_H") == ord("1")
-    assert message.get("UTCTime_Day_L") == ord("6")
-    assert message.get("UTCTime_Hour_H") == ord("0")
-    assert message.get("UTCTime_Hour_L") == ord("3")
-    assert message.get("UTCTime_Minute_H") == ord("0")
-    assert message.get("UTCTime_Minute_L") == ord("2")
-    assert message.get("UTCTime_Second_H") == ord("1")
-    assert message.get("UTCTime_Second_L") == ord("0")
-    assert message.get("UTCTime_Zulu") == ord("Z")
+    assert message.get("U_Year_H") == ord("1")
+    assert message.get("U_Year_L") == ord("9")
+    assert message.get("U_Month_H") == ord("1")
+    assert message.get("U_Month_L") == ord("2")
+    assert message.get("U_Day_H") == ord("1")
+    assert message.get("U_Day_L") == ord("6")
+    assert message.get("U_Hour_H") == ord("0")
+    assert message.get("U_Hour_L") == ord("3")
+    assert message.get("U_Minute_H") == ord("0")
+    assert message.get("U_Minute_L") == ord("2")
+    assert message.get("U_Second_H") == ord("1")
+    assert message.get("U_Second_L") == ord("0")
+    assert message.get("U_Zulu") == ord("Z")
 
 
 def test_parse_generalizedtime_message() -> None:
-    message = ASN1["Message"]
+    message = ASN1["GeneralizedTime"]
     date = "15860923175628Z"
-    message.parse(bytes([0x18, len(date)]) + date.encode("ascii"))
+    message.parse(bytes([len(date)]) + date.encode("ascii"))
     fields = [
-        "GeneralizedTime_G_Century_H",
-        "GeneralizedTime_G_Century_L",
-        "GeneralizedTime_G_Year_H",
-        "GeneralizedTime_G_Year_L",
-        "GeneralizedTime_G_Month_H",
-        "GeneralizedTime_G_Month_L",
-        "GeneralizedTime_G_Day_H",
-        "GeneralizedTime_G_Day_L",
-        "GeneralizedTime_G_Hour_H",
-        "GeneralizedTime_G_Hour_L",
-        "GeneralizedTime_G_Minute_H",
-        "GeneralizedTime_G_Minute_L",
-        "GeneralizedTime_G_Second_H",
-        "GeneralizedTime_G_Second_L",
-        "GeneralizedTime_G_Zulu",
+        "G_Century_H",
+        "G_Century_L",
+        "G_Year_H",
+        "G_Year_L",
+        "G_Month_H",
+        "G_Month_L",
+        "G_Day_H",
+        "G_Day_L",
+        "G_Hour_H",
+        "G_Hour_L",
+        "G_Minute_H",
+        "G_Minute_L",
+        "G_Second_H",
+        "G_Second_L",
+        "G_Zulu",
     ]
     assert [message.get(f) for f in fields] == [ord(d) for d in date]
 
 
 def test_parse_strings() -> None:
     def assert_string(message: MessageValue, encoding: str, expected: str) -> None:
-        data = message.get("Data")
-        assert isinstance(data, list)
-        assert [d.value for d in data] == list(expected.encode(encoding))
+        value = message.get("Value")
+        assert isinstance(value, bytes)
+        assert value == expected.encode(encoding)
 
-    message = ASN1["Message"]
+    message = ASN1["PrintableString"]
 
-    message.parse(bytes([0x13, 0x02, 0x68, 0x69]))
+    message.parse(bytes([0x02, 0x68, 0x69]))
     assert_string(message, "ascii", "hi")
 
-    message.parse(bytes([0x16, 0x02, 0x68, 0x69]))
+    message = ASN1["UTF8String"]
+
+    message.parse(bytes([0x02, 0x68, 0x69]))
     assert_string(message, "ascii", "hi")
 
-    message.parse(bytes([0x0C, 0x04, 0xF0, 0x9F, 0x98, 0x8E]))
+    message.parse(bytes([0x04, 0xF0, 0x9F, 0x98, 0x8E]))
     assert_string(message, "utf-8", "😎")
 
 
 def test_parse_boolean() -> None:
-    message = ASN1["Message"]
-    message.parse(bytes([1, 1, 0]))
-    assert message.get("Bool") == "B_FALSE"
-    message.parse(bytes([1, 1, 0xFF]))
-    assert message.get("Bool") == "B_TRUE"
+    message = ASN1["BOOL"]
+    message.parse(bytes([1, 0]))
+    assert message.get("Value") == "B_FALSE"
+    message.parse(bytes([1, 0xFF]))
+    assert message.get("Value") == "B_TRUE"
     with pytest.raises(ValueError):
-        message.parse(bytes([1, 1, 0x14]))
-
-
-def test_parse_bit_string() -> None:
-    message = ASN1["BIT_STRING"]
-    message.parse(bytes([3, 4, 0, 1, 2, 3]))
-    data = message.get("Data")
-    assert isinstance(data, list)
-    assert [d.value for d in data] == [1, 2, 3]
+        message.parse(bytes([1, 0x14]))
